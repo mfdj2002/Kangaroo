03a3ebf9363934e3cd418d09caffddef80648733
jingkaifu <jf3516@columbia.edu>
2024-08-06 23:26:40 +0800

    forgot to commit new lora

--- kangaroo/lora.py
+++ kangaroo/lora.py
@@ -1,9 +1,16 @@
-from kangaroo.adapter import AdapterModel
+import torch.nn as nn
+from transformers.models.llama.modeling_llama import LlamaDecoderLayer, LlamaRMSNorm
 
-class Lora(AdapterModel):
+class Lora(nn.Module):
     def __init__(self, config):
-        super().__init__(config)
+        super().__init__()
         self.config = config
+        self.gradient_checkpointing = True
+        self.padding_idx = config.pad_token_id
+        self.vocab_size = config.vocab_size
+
+        self.norm = LlamaRMSNorm(config.hidden_size, eps=config.rms_norm_eps)
+        self.layers = nn.ModuleList([LlamaDecoderLayer(config) for _ in range(config.num_hidden_layers)])
     def prepare_inputs_for_generation(self, input_ids, **kwargs):
         return {
             "input_ids": input_ids,
